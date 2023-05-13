c634f26b90ceb5226b177b6347f1cac7b07054b2
AnindhaxNill <anindha004@gmail.com>
2023-05-13 06:56:45 +0600

    tasks done

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path,include
+from .views import *
+
+urlpatterns = [
+    path("register/", UserRegistrationView.as_view(),name='register'),
+    path("login/", UserLoginView.as_view(),name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+
+
+
+]
